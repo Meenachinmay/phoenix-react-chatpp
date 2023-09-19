fd4ba2c8faed393fbe8e35c72e747481cb4a3f62
meenachinmay <chinmayanand896@gmail.com>
2023-09-19 14:25:01 +0900

    added chat layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
+import { Flex, Input, Button } from "@chakra-ui/react";
 import "./App.css";
 
 import { Socket } from "phoenix";
 
 function App() {
-  const [messages, setMessage] = useState([])
-  
+  const [messages, setMessage] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
+
   useEffect(() => {
     const socket = new Socket("ws:localhost:4000/socket", {
       params: { token: "chinmay 123" },
@@ -21,19 +23,65 @@ function App() {
         console.log("Unable to join", resp);
       });
 
-      channel.push("ping", { body: "A client is connected..."})
+    // confirm is client connected
+    channel.push("ping", { body: "A client is connected..." });
 
-      channel.push("new_message", { body: "Chinmay is saying hello..."})
+    // load messages from phoenix server
+    channel.on("new_message", (payload) => {
+      console.log("Reponse from phoenix backend", payload.body);
+    });
 
-      channel.on("new_message", payload => {
-        console.log('Reponse from phoenix backend', payload.body);
-      })
-
-      return () => channel.leave();
+    // disconnect the connection as soon as component unmount
+    return () => channel.leave();
   }, []);
+
+  function handleSendMessage() {
+    console.log("message", newMessage);
+    setNewMessage('');
+  }
+
   return (
     <>
-      <div>Hello world</div>
+      <Flex
+        alignItems={"center"}
+        justifyContent={"center"}
+        width={"full"}
+        height={"100vh"}
+        bg={"orange.300"}
+        flexDir={"column"}
+      >
+        <Flex
+          width={"600px"}
+          height={"600"}
+          bg={"blue.500"}
+          borderRadius={"10px"}
+          padding={"10"}
+        ></Flex>
+        <Flex
+          width={"600px"}
+          height={"70px"}
+          marginTop={"5"}
+          borderRadius={"10px"}
+          gap={10}
+        >
+          <Input
+            width={"full"}
+            type="text"
+            placeholder="Enter your messages here..."
+            padding={"5px"}
+            onChange={(e) => setNewMessage(e.target.value)}
+            value={newMessage}
+          />
+          <Button
+            bg={"white"}
+            p={"5px"}
+            color={"gray.500"}
+            onClick={handleSendMessage}
+          >
+            Send
+          </Button>
+        </Flex>
+      </Flex>
     </>
   );
 }
